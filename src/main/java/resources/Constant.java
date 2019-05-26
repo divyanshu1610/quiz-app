a5daa9ec809d12d3865511af5199a51782155238
divyanshu1610 <divyanshu1610@gmail.com>
2019-05-26 15:17:58 +0530

    connection str removed

--- src/main/java/resources/Constant.java
+++ src/main/java/resources/Constant.java
@@ -10,7 +10,8 @@ public final class  Constant {
     
    public final static int RESULT_OK =119;
    public final static int  RESULT_NOK =186;
-   public final static String ALPHA ="mongodb://divyanshu:Divyanshu16@justquiz-shard-00-00-jujli.gcp.mongodb.net:27017,justquiz-shard-00-01-jujli.gcp.mongodb.net:27017,justquiz-shard-00-02-jujli.gcp.mongodb.net:27017/test?ssl=true&replicaSet=JustQuiz-shard-0&authSource=admin&retryWrites=true";
+   //MongoDB connection string
+   public final static String ALPHA ="REPLACE THIS BY YOUR CONNECTION STRING" ;
    public final static String AUTHENTICATION ="authentication";
    public final static String S_RECORD ="studentRecord";
     public final static String STUDENT ="STUDENT";
